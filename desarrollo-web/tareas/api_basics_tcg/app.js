49a8e608b6edb787c2f12c99e9c46eee54b0ed4b
Nicole Dávila <a01784217@tec.mx>
2024-04-29 09:08:43 -0600

    added function that adds cards to the cards list

--- desarrollo-web/tareas/api_basics_tcg/app.js
+++ desarrollo-web/tareas/api_basics_tcg/app.js
@@ -10,16 +10,31 @@ let cards = []; // stores cards
 // returns existing cards (or an error message if none exist)
 app.get('/cards', (req, res) => {
     if (cartas.length === 0) {
-        res.status(404).json('you have no cards yet!');
+        res.status(404).send('you have no cards yet!');
     } else
-  res.json(cards);
+  res.send(cards);
 });
+
 // gets a card by its id
 app.get('/cards/:id', (req, res) => {
     const card = cards.find(card => card.id === parseInt(req.params.id));
     if (!card) {
-        res.status(404).json('card not found');
+        res.status(404).send('card not found');
     } else
-  res.json(card);
+  res.send(card);
 });
 
+/*receives a json with the new cards that will be added to the list.
+checks that the json has all the necessary attributes or that the card doesn exist yet,
+or it throws a 404 error. if the carrd was added successfully, it also send a success message */
+app.post('/cards', (req, res) => {
+    const { id, name, type, description } = req.body;
+    if (!id || !name || !type || !description) {
+        res.status(404).send('missing attributes');
+    } else if (cards.find(card => card.id === id)) {
+        res.status(404).send('card already exists');
+    } else {
+        cards.push({ id, name, type, description });
+        res.status(201).send('card added successfully');
+    }
+});
